89c25f1b7c25783794567d6763bd6e7b98030329
Jeffrey DeYoung <jeffrey.j.deyoung@gmail.com>
2019-10-21 19:48:38 -0600

    master -- start of work on sensor/actor node stuff in Python

---
+++ RaspberryPi/pi-api.py
@@ -0,0 +1,9 @@
+#This is the entry point for our RaspberryPi based Python API for communicating sensor data and taking action changing relays/switches/valves/pumps/etc
+#Also, this may be the best file name I've ever had in an application.
+
+
+def main():
+    print "Starting up our PI API..."
+
+if __name__ == '__main__':
+    main()
