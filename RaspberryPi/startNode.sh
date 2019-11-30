0b078e8304624e027acfa529c3fec9b3a99cc005
Jeffrey DeYoung <jeffrey.j.deyoung@gmail.com>
2019-11-30 12:16:37 -0700

    master -- better start script?

--- RaspberryPi/startNode.sh
+++ RaspberryPi/startNode.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Starting up the JavaHVAC Raspberry PI Service..."
+if [ ! -d ".venv" ]
+then
+  echo "Virtual environment does not yet exist. Creating now..."
+  virtualenv .venv
+  echo "Installing dependencies to virtual environment..."
+  .venv/bin/pip install -r requirements.txt
+  echo "Done setting up Virtual environment.,"
+fi
+
 .venv/bin/python pi-api.py
