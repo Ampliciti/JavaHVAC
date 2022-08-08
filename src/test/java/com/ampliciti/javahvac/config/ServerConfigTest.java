1f97f447ba33b340042ee308ebfcf0d1c5550d71
Jeffrey DeYoung <jeffrey.j.deyoung@gmail.com>
2022-08-07 18:40:18 -0600

    configure-server-port -- added logging for python side, port is now configurable

--- src/test/java/com/ampliciti/javahvac/config/ServerConfigTest.java
+++ src/test/java/com/ampliciti/javahvac/config/ServerConfigTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 jeffrey
+ * Copyright (C) 2018-2022 jeffrey
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
@@ -87,6 +87,7 @@ public class ServerConfigTest {
 
     assertEquals(new Double(39.0000), ServerConfig.getLocation().getLatitude());
     assertEquals(new Double(-104.0000), ServerConfig.getLocation().getLongitude());
+    assertEquals((Integer) 8080, (Integer) ServerConfig.getPort());
 
   }
 
