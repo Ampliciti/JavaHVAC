b232fbfca0d39f74199de3b8953758b1efef0878
Jeffrey DeYoung <jeffrey.j.deyoung@gmail.com>
2024-07-05 15:10:02 -0600

    python-upgrade -- more pyton3 fixes

--- JavaHVAC-Node/GPIOHelper.py
+++ JavaHVAC-Node/GPIOHelper.py
@@ -1,7 +1,7 @@
 try:
     import RPi.GPIO as GPIO
 except:
-    print "Unable to load GPIO lib; this is probaly not a RaspberryPi. Continuing without GPIO support."
+    print("Unable to load GPIO lib; this is probaly not a RaspberryPi. Continuing without GPIO support.")
 
 
 #consturctor
@@ -12,26 +12,26 @@ def setup(pins):
         GPIO.setmode(GPIO.BOARD)
         #only supporting output at this time (ie, relays or other binary outputs); default to off
         for pin in pins:
-            print "Trying to setup pin " + str(pin) + "..."
+            print("Trying to setup pin " + str(pin) + "...")
             if pin["invert"]:
                 GPIO.setup(pin["pin"], GPIO.OUT, initial=GPIO.HIGH)
             else:
                 GPIO.setup(pin["pin"], GPIO.OUT, initial=GPIO.LOW)
 
     except NameError:
-        print "Unable to setup GPIO; this is probaly not a RaspberryPi. Continuing without GPIO support."
+        print("Unable to setup GPIO; this is probaly not a RaspberryPi. Continuing without GPIO support.")
 
 def getPinState(pin):
-    print "Getting state for GPIO pin: " + str(pin)
+    print("Getting state for GPIO pin: " + str(pin))
     if GPIO.input(int(pin)):
         state = True
     else:
         state = False
-    print "State for pin: " + str(pin) + " is: " + str(state)
+    print("State for pin: " + str(pin) + " is: " + str(state))
     return state
 
 def setPinState(pin, value):
-    print "Setting state for GPIO pin: " + str(pin) + ", value: " + str(value)
+    print("Setting state for GPIO pin: " + str(pin) + ", value: " + str(value))
     if value:
         GPIO.output(int(pin), GPIO.HIGH)
     else:
